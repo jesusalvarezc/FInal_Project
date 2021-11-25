2c3785273eec2e9ba39d9636ee67e030992e2690
Jesus Alvarez <alvcasjes@hotmail.com>
2021-11-25 09:51:21 -0600

    look_at_change func added

--- data.py
+++ data.py
@@ -10,6 +10,8 @@
 """
 
 import pandas as pd
+import datetime as dt
+import numpy as np
 
 def peso_usd(amount):
     return 1/amount
@@ -26,12 +28,27 @@ def f_leer_archivos(file):
 def f_leer_archivos_varios(*files):
     result = pd.DataFrame()
     for file in files:
-        data = pd.read_excel("0files/"+file)
-        data.close = data.close.apply(peso_usd)
-        data.open = data.open.apply(peso_usd)
-        high1 = data.low.apply(peso_usd)
-        low1 = data.high.apply(peso_usd)
-        data.high = high1
-        data.low = low1
+        data = pd.read_csv("0files/"+file)
         result = pd.concat([result, data])
+    result.close = np.divide(1, result.close)
+    result.open = np.divide(1, result.open)
+    high1 = np.divide(1, result.low)
+    low1 = np.divide(1, result.high)
+    result.high = high1
+    result.low = low1
+    result.timestamp = [dt.datetime.strptime(i, "%d/%m/%y %H:%M") for i in result.timestamp]
+
     return result
+
+
+def look_at_change(data_ind: pd.DataFrame, data_change: pd.DataFrame):
+    res = {}
+    for i in range(len(data_ind)):
+        dic_ind = data_ind.iloc[i,0]
+        delta = dt.timedelta(minutes=30)
+        start = dic_ind - delta
+        end = dic_ind + delta
+        res_to_append = data_change[(data_change["timestamp"] <= end) & (data_change["timestamp"] >= start)]
+        res[dic_ind] = res_to_append
+        
+    return res
--- functions.py
+++ functions.py
@@ -13,3 +13,21 @@
 
 
 
+# Computational
+
+def clasificacion(data):
+    res = []
+    for i in range(len(data)):
+        actual = data.iloc[i,1]
+        consensus = data.iloc[i,2]
+        previous = data.iloc[i,3]
+        if actual >= consensus >= previous:
+            res.append("A")
+        elif actual >= consensus < previous:
+            res.append("B")
+        elif actual < consensus >= previous:
+            res.append("C")
+        elif actual < consensus < previous:
+            res.append("D")
+    data["Escenario"] = res
+    return data
