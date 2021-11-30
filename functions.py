ef626484a5cbd438b9aada355df2677876d1c932
Rodo Slay <if715214@iteso.mx>
2021-11-29 19:29:50 -0600

    Statistics Fn
    Almost done, add to notebook

--- functions.py
+++ functions.py
@@ -11,6 +11,62 @@
 
 # Statistical
 
+import statsmodels.api as sm
+from scipy import stats
+from pylab import rcParams
+from statsmodels.tsa.stattools import adfuller
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf
+
+
+def linea(data):
+    fig = px.line(data, x='DateTime', y='Actual')
+    return fig.show()
+
+
+# A y AP
+def autocorr(data):
+    A = plot_acf(data.Actual)
+    AP = plot_pacf(data.Actual)
+    return A, AP
+
+
+# prueba heterocedasticidad
+def hetero(data):
+    fig, ax = plt.subplots(1,1, figsize=[12,6])
+    a = sm.qqplot(data.Actual, line= 'q', fit  = True, ax = ax)
+
+
+# Normalidad
+def normalidad(data):
+    print('Kursotis:', stats.kurtosis(data.Actual))
+    print('Skewness:', stats.skew(data.Actual))
+    print('Shapiro-Wilk: ', stats.shapiro(data.Actual)) # no más de 50 datos
+    print('D Agostino:', stats.normaltest(data.Actual))
+
+
+def estacion(data):
+    # Estacionalidad
+    rcParams['figure.figsize'] = 16, 6
+    decomposition = sm.tsa.seasonal_decompose(data.Actual, model='additive', period=12)
+    decomposition.plot()
+    ## Test de DICKEY-FULLER para Estacionariedad
+    df_test = adfuller(data.Actual)
+    if df_test[1] > 0.05:
+        test= print('No hay evidencia de estacionariedad')
+    else:
+        test= print('Hay evidencia de estacionariedad')
+
+    return print((test,df_test[1])) ,plt.show()
+
+
+# Datos Atípicos
+def atipicos(data):
+    ax = sns.boxplot(x='Actual', data=data)
+
 
 
 # Computational
